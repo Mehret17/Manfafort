f76a53315d1a7d8638fb3c3f4e6b468846ab09de
Leo Taylor <leotaylor723@gmail.com>
2018-10-05 09:57:52 -0500

    git github githubbing hubs

--- Manafort/Controllers/IncarceratedController.cs
+++ Manafort/Controllers/IncarceratedController.cs
@@ -37,38 +37,6 @@ namespace Manafort.Controllers
             return Ok(findInterests);
         }
 
-        //[HttpGet("murder")]
-        //public ActionResult<IEnumerable<Incarcerated>> GetMurders()
-        //{
-        //    var allPrisoners = storage.GetAll();
-        //    var murderPrisoners = allPrisoners.Where(inmate => inmate.Interests.ToLower() == "murder");
-        //    return Ok(murderPrisoners);
-        //}
-
-        //[HttpGet("fraud")]
-        //public ActionResult<IEnumerable<Incarcerated>> GetFrauders()
-        //{
-        //    var allPrisoners = storage.GetAll();
-        //    var fraudPrisoners = allPrisoners.Where(inmate => inmate.Interests.ToLower() == "fraud");
-        //    return Ok(fraudPrisoners);
-        //}
-
-        //[HttpGet("stealing")]
-        //public ActionResult<IEnumerable<Incarcerated>> GetStealers()
-        //{
-        //    var allPrisoners = storage.GetAll();
-        //    var stealingPrisoners = allPrisoners.Where(inmate => inmate.Interests.ToLower() == "stealing");
-        //    return Ok(stealingPrisoners);
-        //}
-
-        //[HttpGet("chess")]
-        //public ActionResult<IEnumerable<Incarcerated>> GetChessers()
-        //{
-        //    var allPrisoners = storage.GetAll();
-        //    var chessPrisoners = allPrisoners.Where(inmate => inmate.Interests.ToLower() == "chess");
-        //    return Ok(chessPrisoners);
-        //}
-
         [HttpGet("{prisonerNo}/services")]
         public ActionResult<IEnumerable<Incarcerated>> GetServices(int prisonerNo, Services services)
         {
